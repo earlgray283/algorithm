0ac564f7efefd7d06de369ade1670b882991f023
earlgray283 <earlgray283@gmail.com>
2020-03-05 00:39:17 +0900

    剰余あたりを修正

--- nCr.c
+++ nCr.c
@@ -2,9 +2,9 @@
 long long modpow(long long a,long long n){
     long long res=1;
     while(n>0){
-        if(n&1)res=res*a%MOD;
-        a=a*a%MOD;
-        n=n>>1;
+        if(n&1){res*=a;res%=MOD;}
+        a*=a;a%=MOD;
+        n>>=1;
     }
     return res;
 }   
@@ -13,6 +13,5 @@ long long nCr(long long n,long long r){
     res=x=y=1;
     for(int i=n;i>n-r;i--){x*=i;x%=MOD;}
     for(int i=r;i>0;i--){y*=i;y%=MOD;}
-    printf("%lld %lld\n",x,y);
     return x*modpow(y,MOD-2)%MOD;
 }
